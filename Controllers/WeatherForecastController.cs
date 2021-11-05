4b191f6a38ef3ecac9256a3316b895d7626f0664
Roberto Borbolla <irob263@gmail.com>
2021-11-05 03:11:07 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wsVentaReal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            List<WeatherForecast> list = new List<WeatherForecast>();
+
+            list.Add(new WeatherForecast() { Id = 5, Nombre = "Roberto Borbolla" });
+            list.Add(new WeatherForecast() { Id = 2, Nombre = "Alejandra Trejo" });
+
+            return list;
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wsVentaReal
+{
+    public class WeatherForecast
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
